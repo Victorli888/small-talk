6e5590090006552337be0d9a82867a646c609ee3
victorli888 <victorli7462@gmail.com>
2024-12-24 19:28:11 -0800

    small-talk logo

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,17 +12,15 @@ export default function Home() {
     };
 
     return (
-        <div class="px-4 py-8 mx-auto bg-[#86efac]">
+        <div class="px-4 py-8 mx-auto bg-[#86efacff]">
             <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
                 <img
                     class="my-6"
-                    src="/logo.svg"
-                    width="128"
-                    height="128"
+                    src="/SmallTalkLogo.svg"
+                    width="256"
+                    height="256"
                     alt="the Fresh logo: a sliced lemon dripping with juice"
                 />
-                <h1 class="text-4xl font-bold">Let's Make Small Talk</h1>
-
                 {/* Chat Component */}
                 <div class="w-full max-w-md mt-8 border rounded-lg shadow-lg">
                     <ChatHistory
