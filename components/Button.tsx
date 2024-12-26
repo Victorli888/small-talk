6c3e4ca23597f1f693d9918af0ff2eec81459309
victorli <victorli7462@gmail.com>
2024-12-25 21:06:24 -0800

    Remove comments for Generic Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,8 +6,7 @@ export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      // class="px-2 py-1 border-gray-500 border-2 rounded-r-lg bg-blue-500 hover:bg-gray-200 transition-colors"
-      class="bg-blue-500 text-white px-5 py-2 rounded-r-lg hover:bg-gray-500"
+      class="bg-blue-500 text-white px-5 py-2 rounded-r-lg hover:bg-blue-600"
     />
   );
 }
